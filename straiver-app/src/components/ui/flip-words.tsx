1710edd3c0199f5de17d471e67a7033f4925f696
zesty-space67 <nehameethjain@gmail.com>
2025-06-25 11:09:44 +0530

    Unused imports removed

--- straiver-app/src/components/ui/flip-words.tsx
+++ straiver-app/src/components/ui/flip-words.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { AnimatePresence, motion, LayoutGroup } from "motion/react";
+import  { useCallback, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 import { cn } from "../../lib/utils";
 
 export const FlipWords = ({
